5301acfad8d2a4b49bee74daa22d0901e45e2dc4
Brandon Deaquino Tototzintle <deaquinobrandon5@gmail.com>
2026-06-26 00:18:11 -0600

    Ruby: Versión 2 - Traducción del resultado a español con Google Translate API (net/http)

--- app.rb
+++ app.rb
@@ -1,4 +1,36 @@
 require 'sinatra'
+require 'net/http'
+require 'uri'
+require 'json'
+
+def traducir_con_google(texto)
+  return texto if texto.nil? || texto.empty?
+  
+  begin
+    #USANDO LA API DE GOOGLE TRANSLATE
+    url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=es&dt=t&q=#{URI.encode_www_form_component(texto)}"
+    
+    uri = URI.parse(url)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    
+    request = Net::HTTP::Get.new(uri.request_uri)
+    response = http.request(request)
+    
+    if response.code == "200"
+      data = JSON.parse(response.body)
+      if data && data[0]
+        traduccion = data[0].map { |item| item[0] }.join
+        return traduccion
+      end
+    end
+    return texto
+  rescue => e
+    puts "Error en traducción: #{e.message}"
+    return texto
+  end
+end
 
 get '/' do
   num = params['n'].to_i
@@ -24,7 +56,8 @@ get '/' do
   if response && !response.empty?
     match = response.match(/<[^>]*NumberToWordsResult[^>]*>(.*?)<\/[^>]*NumberToWordsResult>/m)
     if match
-      return match[1].strip
+      resultado_ingles = match[1].strip
+      return traducir_con_google(resultado_ingles)
     else
       return "No se encontró resultado"
     end
