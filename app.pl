1fa6ce9975840bbc4f6a4e555f930126aea751a1
Brandon Deaquino Tototzintle <deaquinobrandon5@gmail.com>
2026-06-26 21:54:17 -0600

    Perl Versión 3 - Conversión nativa a español (código base del lenguaje)

--- app.pl
+++ app.pl
@@ -2,9 +2,63 @@ use strict;
 use warnings;
 use HTTP::Daemon;
 use HTTP::Status;
-use LWP::UserAgent;
-use WWW::Google::Translate;
-use JSON;
+
+sub numero_a_letras {
+    my ($num) = @_;
+    
+    return "Número inválido" unless defined $num && $num =~ /^\d+$/;
+    
+    my $n = int($num);
+    return "cero" if $n == 0;
+    
+        my @unidades = ('', 'uno', 'dos', 'tres', 'cuatro', 'cinco', 'seis', 'siete', 'ocho', 'nueve');
+    my @especiales = ('diez', 'once', 'doce', 'trece', 'catorce', 'quince', 
+                      'dieciséis', 'diecisiete', 'dieciocho', 'diecinueve');
+    my @decenas = ('', 'diez', 'veinte', 'treinta', 'cuarenta', 'cincuenta',
+                   'sesenta', 'setenta', 'ochenta', 'noventa');
+    my @centenas = ('', 'cien', 'doscientos', 'trescientos', 'cuatrocientos',
+                    'quinientos', 'seiscientos', 'setecientos', 'ochocientos', 'novecientos');
+    
+    if ($n < 10) {
+        return $unidades[$n];
+    }
+    elsif ($n < 20) {
+        return $especiales[$n - 10];
+    }
+    elsif ($n < 30) {
+        return $n == 20 ? "veinte" : "veinti" . $unidades[$n - 20];
+    }
+    elsif ($n < 100) {
+        my $dec = int($n / 10);
+        my $uni = $n % 10;
+        return $uni == 0 ? $decenas[$dec] : $decenas[$dec] . " y " . $unidades[$uni];
+    }
+    elsif ($n < 1000) {
+        my $cent = int($n / 100);
+        my $resto = $n % 100;
+        if ($cent == 1 && $resto == 0) {
+            return "cien";
+        } elsif ($cent == 1) {
+            return "ciento " . numero_a_letras($resto);
+        } elsif ($resto == 0) {
+            return $centenas[$cent];
+        } else {
+            return $centenas[$cent] . " " . numero_a_letras($resto);
+        }
+    }
+    elsif ($n < 10000) {
+        my $mil = int($n / 1000);
+        my $resto = $n % 1000;
+        if ($mil == 1) {
+            return $resto == 0 ? "mil" : "mil " . numero_a_letras($resto);
+        } else {
+            return $resto == 0 ? numero_a_letras($mil) . " mil" : numero_a_letras($mil) . " mil " . numero_a_letras($resto);
+        }
+    }
+    else {
+        return "número fuera de rango (máximo 9999)";
+    }
+}
 
 my $daemon = HTTP::Daemon->new(
     LocalPort => 5000,
@@ -19,53 +73,9 @@ while (my $connection = $daemon->accept) {
         my $query = $uri->query || '';
         my %params = map { split('=', $_) } split('&', $query);
         my $num = $params{'n'};
- 
+        
         if (defined $num && $num ne '') {
-            my $soap_request = qq{<?xml version="1.0" encoding="utf-8"?>
-<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
-  <soap:Body>
-    <NumberToWords xmlns="http://www.dataaccess.com/webservicesserver/">
-      <ubiNum>$num</ubiNum>
-    </NumberToWords>
-  </soap:Body>
-</soap:Envelope>};
- 
-            my $ua = LWP::UserAgent->new;
-            $ua->timeout(60);
-            $ua->ssl_opts( verify_hostname => 0 );
-            
-            my $response = $ua->post(
-                'https://www.dataaccess.com/webservicesserver/NumberConversion.wso',
-                'Content-Type' => 'text/xml; charset=utf-8',
-                'SOAPAction' => 'http://www.dataaccess.com/webservicesserver/NumberToWords',
-                'Content' => $soap_request
-            );
- 
-            my $resultado;
-            if ($response->is_success) {
-                my $content = $response->content;
-                if ($content =~ /<[^>]*NumberToWordsResult[^>]*>(.*?)<\/[^>]*NumberToWordsResult>/) {
-                    my $resultado_ingles = $1;
-                    $resultado_ingles =~ s/^\s+|\s+$//g;
-                    
-                    eval {
-                        my $trans = WWW::Google::Translate->new();
-                        $resultado = $trans->translate(
-                            source => 'en',
-                            target => 'es',
-                            text => $resultado_ingles
-                        );
-                    };
-                    if ($@) {
-                        $resultado = traducir_con_api_directa($resultado_ingles);
-                    }
-                } else {
-                    $resultado = "No se encontró resultado";
-                }
-            } else {
-                $resultado = "Error HTTP: " . $response->status_line;
-            }
- 
+            my $resultado = numero_a_letras($num);
             $connection->send_response(HTTP::Response->new(200, 'OK', ['Content-Type' => 'text/plain'], $resultado));
         } else {
             $connection->send_response(HTTP::Response->new(200, 'OK', ['Content-Type' => 'text/plain'], "Usa: ?n=10"));
@@ -73,32 +83,4 @@ while (my $connection = $daemon->accept) {
     }
     $connection->close;
     undef($connection);
-}
-
-sub traducir_con_api_directa {
-    my ($texto) = @_;
- 
-    my $texto_escaped = $texto;
-    $texto_escaped =~ s/ /%20/g;
-    $texto_escaped =~ s/"/%22/g;
- 
-    my $url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=es&dt=t&q=$texto_escaped";
- 
-    my $ua = LWP::UserAgent->new;
-    $ua->timeout(10);
-    $ua->ssl_opts( verify_hostname => 0 );
-    
-    my $response = $ua->get($url);
- 
-    if ($response->is_success) {
-        my $data = decode_json($response->content);
-        if ($data && ref($data) eq 'ARRAY' && $data->[0]) {
-            my $traduccion = '';
-            foreach my $item (@{$data->[0]}) {
-                $traduccion .= $item->[0];
-            }
-            return $traduccion;
-        }
-    }
-    return $texto;  
 }
