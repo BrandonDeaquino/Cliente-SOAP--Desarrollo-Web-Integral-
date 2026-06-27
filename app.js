c77aec7f28b58d570faf803f0ed4b38639836c53
Brandon Deaquino Tototzintle <deaquinobrandon5@gmail.com>
2026-06-26 22:26:15 -0600

    Node Versión 2 - Traducción del resultado a español con @vitalets/google-translate-api

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const http = require('http');
 const url = require('url');
 const soap = require('soap');
+const { translate } = require('@vitalets/google-translate-api');
 
 const server = http.createServer((req, res) => {
-    const query = url.parse(req.url, true).query;
-    const num = query.n;
+    const query = new URLSearchParams(req.url.split('?')[1] || '');
+    const num = query.get('n');
     
     if (!num) {
         res.writeHead(200, { 'Content-Type': 'text/plain' });
@@ -21,19 +22,25 @@ const server = http.createServer((req, res) => {
             return;
         }
         
-        client.NumberToWords({ ubiNum: num }, (err, result) => {
+        client.NumberToWords({ ubiNum: num }, async (err, result) => {
             if (err) {
                 res.writeHead(500, { 'Content-Type': 'text/plain' });
                 res.end('Error al llamar al servicio SOAP: ' + err.message);
                 return;
             }
             
-            res.writeHead(200, { 'Content-Type': 'text/plain' });
-            res.end(result.NumberToWordsResult);
+            try {
+                const translation = await translate(result.NumberToWordsResult, { to: 'es' });
+                res.writeHead(200, { 'Content-Type': 'text/plain' });
+                res.end(translation.text);
+            } catch (err) {
+                console.error('Error en traducción:', err.message);
+                res.writeHead(200, { 'Content-Type': 'text/plain' });
+                res.end(result.NumberToWordsResult);
+            }
         });
     });
 });
-
 server.listen(3000, () => {
     console.log('Servidor iniciado en http://localhost:3000');
 });
