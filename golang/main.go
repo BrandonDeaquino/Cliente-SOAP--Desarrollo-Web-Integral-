5f2fa2d8fe6a1a50022eabd45e306af54a311772
Brandon Deaquino Tototzintle <deaquinobrandon5@gmail.com>
2026-06-27 01:29:47 -0600

    Golang Versión 2 - Traducción a español con librerías(net/http + encoding/json)

--- golang/main.go
+++ golang/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"encoding/xml"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"net/url"
 	"github.com/hooklift/gowsdl/soap"
 )
 
@@ -45,8 +48,46 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(fmt.Sprintf("Error: %s", err.Error())))
 		return
 	}
+	resultadoEspanol := traducirConGoogle(response.Result)
 	
-	w.Write([]byte(response.Result))
+	w.Write([]byte(resultadoEspanol))
+}
+
+func traducirConGoogle(texto string) string {
+	url := fmt.Sprintf("https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=es&dt=t&q=%s", url.QueryEscape(texto))
+	
+	resp, err := http.Get(url)
+	if err != nil {
+		return texto
+	}
+	defer resp.Body.Close()
+	
+	body, _ := ioutil.ReadAll(resp.Body)
+	
+	var data []interface{}
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		return texto
+	}
+	if len(data) > 0 {
+		firstArray, ok := data[0].([]interface{})
+		if ok && len(firstArray) > 0 {
+			var traduccion string
+			for _, item := range firstArray {
+				itemArray, ok := item.([]interface{})
+				if ok && len(itemArray) > 0 {
+					if str, ok := itemArray[0].(string); ok {
+						traduccion += str
+					}
+				}
+			}
+			if traduccion != "" {
+				return traduccion
+			}
+		}
+	}
+	
+	return texto
 }
 
 func parseInt(s string) int {
